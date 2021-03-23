b19ecc982a805d8883d0288fdac4684226bb4f26
deundrewilliams <dnamedno17@gmail.com>
2021-03-23 13:41:00 -0400

    update build

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('frontend/build'));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
     });
   }
