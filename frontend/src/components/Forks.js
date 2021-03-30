abb9c01fe829043d372a13057fe2910c4a89500a
deundrewilliams <dnamedno17@gmail.com>
2021-03-29 20:02:07 -0400

    add num formatting

--- frontend/src/components/Forks.js
+++ frontend/src/components/Forks.js
@@ -5,7 +5,7 @@ function Forks(props) {
     return(
         <div className="forks">
             <img src="https://img.icons8.com/ios/100/000000/code-fork.png" alt="fork icon"/>
-            <p>{fork_count} forks</p>
+            <p>{Number(fork_count).toLocaleString()} forks</p>
         </div>
     )
 
